94f35827967680e8ad6c6b3f1d8b2d0dec92ee65
vishesh13byte <visheshrustagi8@gmail.com>
2022-08-23 15:36:32 +0530

    made some changes

--- Nagarro-Training-main/pokemon assignment/script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById("bt");
-
-btn.addEventListener("click", function (e) {
-  if (!confirm("Are you sure you want to visit Pokémon Wiki?")) {
-    e.preventDefault();
-  }
-});
