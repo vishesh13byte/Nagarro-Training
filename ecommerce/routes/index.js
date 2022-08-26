36dc3ef1cd36e10a2ee87b2c1684a6e91f5caa57
vishesh13byte <visheshrustagi8@gmail.com>
2022-08-26 12:45:39 +0530

    final

---
+++ ecommerce/routes/index.js
@@ -0,0 +1,49 @@
+var express = require("express");
+var router = express.Router();
+var passport = require("passport");
+var User = require("../models/user");
+
+router.get("/", function (req, res) {
+  res.redirect("/home");
+});
+
+router.get("/register", function (req, res) {
+  res.render("index/register");
+});
+
+router.post("/register", function (req, res) {
+  var newUser = new User({ username: req.body.username });
+  User.register(newUser, req.body.password, function (err, user) {
+    if (err) {
+      req.flash("error", err.message);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        req.flash("success", "Hi " + user.username + ", Welcome to My Store!");
+        res.redirect("/home");
+      });
+    }
+  });
+});
+
+router.get("/login", function (req, res) {
+  res.render("index/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/home",
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  function (req, res) {}
+);
+
+router.get("/logout", function (req, res) {
+  req.logout();
+  req.flash("success", "Logged you out!!");
+  res.redirect("/home");
+});
+
+module.exports = router;
